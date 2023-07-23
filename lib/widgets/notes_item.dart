9e20d017381691371f2f5597dbfc12f1abc36551
ahmed.magdy <ahmed.magdy@gmail.com>
2023-07-24 01:13:17 +0300

    create note sview body

---
+++ lib/widgets/notes_item.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+Widget noteItem() => Container(
+      padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 16),
+      decoration: BoxDecoration(
+        color: Colors.cyan,
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          ListTile(
+            title: Text(
+              'Flutter Tips',
+              style: TextStyle(color: Colors.black, fontSize: 26),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 20),
+              child: Text(
+                'Build your career with tharwat samy',
+                style: TextStyle(
+                    color: Colors.black.withOpacity(0.4), fontSize: 20),
+              ),
+            ),
+            trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  FontAwesomeIcons.trash,
+                  color: Colors.black,
+                )),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              'July23,  2023',
+              style:
+                  TextStyle(color: Colors.black.withOpacity(0.4), fontSize: 16),
+            ),
+          ),
+        ],
+      ),
+    );
