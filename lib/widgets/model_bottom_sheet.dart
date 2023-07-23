dbfb946e9d12ad89a1cb6f5771f277c59ab6c1f2
ahmed.magdy <ahmed.magdy@gmail.com>
2023-07-24 02:22:45 +0300

    create constants color

--- lib/widgets/model_bottom_sheet.dart
+++ lib/widgets/model_bottom_sheet.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
 
-Widget addBottomSheet() => Container();
+Widget addBottomSheet() => Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
