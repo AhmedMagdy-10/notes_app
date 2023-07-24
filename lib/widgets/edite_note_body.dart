2b944c3e7650a1ad7645e582a2cfc91406347017
ahmed.magdy <ahmed.magdy@gmail.com>
2023-07-24 18:39:32 +0300

    edite note view ui done

--- lib/widgets/edite_note_body.dart
+++ lib/widgets/edite_note_body.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:notes/widgets/custom_appBar.dart';
+import 'package:notes/widgets/custom_text_field.dart';
 
 Widget editNoteBody() => Padding(
       padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 50,
           ),
           customAppBar(title: 'Edit Note', icon: Icons.check),
+          const SizedBox(
+            height: 50,
+          ),
+          customTextField(hint: 'Titel'),
+          const SizedBox(
+            height: 16,
+          ),
+          customTextField(hint: 'Content', maxLines: 5),
         ],
       ),
     );
