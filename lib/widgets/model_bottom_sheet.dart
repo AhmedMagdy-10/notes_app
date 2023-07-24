b3b25bea5ecf31e5968906b61ef2a041a8f9bb3e
ahmed.magdy <ahmed.magdy@gmail.com>
2023-07-24 06:15:23 +0300

    add custom bottom to bttom sheet

--- lib/widgets/model_bottom_sheet.dart
+++ lib/widgets/model_bottom_sheet.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:notes/widgets/custom_bottom.dart';
 import 'package:notes/widgets/custom_text_field.dart';
 
 Widget addBottomSheet() => Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          const SizedBox(
-            height: 32,
-          ),
-          customTextField(
-            hint: 'Title',
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          customTextField(
-            hint: 'Content',
-            maxLines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 32,
+            ),
+            customTextField(
+              hint: 'Title',
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            customTextField(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            customBottom(),
+            const SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
