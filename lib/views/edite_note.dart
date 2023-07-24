a8964eee80c267df8901fb2bd30c283078673f59
ahmed.magdy <ahmed.magdy@gmail.com>
2023-07-24 18:03:10 +0300

    create edite view

---
+++ lib/views/edite_note.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/edite_note_body.dart';
+
+class editNoteView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: editNoteBody(),
+    );
+  }
+}
