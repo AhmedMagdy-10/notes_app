6866a3c2a319efe287cdb0fc00d9a273c6261c4c
ahmed.magdy <ahmed.magdy@gmail.com>
2023-07-25 15:10:40 +0300

    add notes States

--- lib/cubits/add_note_cubit/add_notes_states.dart
+++ lib/cubits/add_note_cubit/add_notes_states.dart
@@ -1,3 +1,13 @@
 abstract class NoteStates {}
 
 class InitailState extends NoteStates {}
+
+class LoadingState extends NoteStates {}
+
+class SuccessState extends NoteStates {}
+
+class ErrorState extends NoteStates {
+  final String errormessage;
+
+  ErrorState(this.errormessage);
+}
